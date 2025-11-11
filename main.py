79b23e846400220e1fcf8185e55fd999d0162848
sweetnsoya <sweetnsoya.ad@gmail.com>
2025-11-11 22:20:23 +0000

    added player.py and circleshape.py. Updated constants.py with additional variables. Updated main.py to show the location of the player

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from circleshape import CircleShape
+from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     print(f"Starting Asteroids! with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    log_state()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
+
+        log_state()
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
